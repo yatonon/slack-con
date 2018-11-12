5304be100fbcf39fad5555fb598b479fdd31f2c2
yatonon <yatonon@yahoo.co.jp>
2018-11-12 17:18:13 +0900

    とりあえず使える

--- app/commit_receiver.rb
+++ app/commit_receiver.rb
@@ -4,6 +4,7 @@ require 'tapp'
 require 'date'
 require 'dotenv'
 
+# コメント取ってくる
 class CommitReceiver
   attr_accessor :message
   def initialize
@@ -11,13 +12,22 @@ class CommitReceiver
     Dotenv.load
     client = Octokit::Client.new login: ENV['LOGIN'], password:ENV['PASSWORD']
     commitlist = client.commits ENV['REPOSITORI']
-    commitlist
+    if Date.today.wday == 1
+      for num in 1..3 do
+        commitlist
+          .select { |commit| commit[:commit][:author][:date].to_date == Date.today-num }
+          .each.with_index(1) do |commit, index|
+            @message << index.to_s + '.' + commit[:commit][:message].gsub(/\*(.*?)\n/,'') \
+            .gsub(/(\r\n?|\n)/,"") +'(' + commit[:author][:login] + ")\n"
+          end
+      end
+    else
+      commitlist
       .select { |commit| commit[:commit][:author][:date].to_date == Date.today-1 }
       .each.with_index(1) do |commit, index|
-        @message << index.to_s + '.' + commit[:commit][:message] \
-        + '(' + commit[:author][:login] + ")\n"
+        @message << index.to_s + '.' + commit[:commit][:message].gsub(/\*(.*?)\n/,'') \
+        .gsub(/(\r\n?|\n)/,"") +'(' + commit[:author][:login] + ")\n"
       end
+    end
   end
 end
-
-# .select { |commit| commit[:commit][:author][:date].to_date == Date.today-1||commit[:commit][:author][:date].to_date-1 == Date.today-2 ||commit[:commit][:author][:date].to_date-2 == Date.today-3}
--- app/slackbot.rb
+++ app/slackbot.rb
@@ -1,13 +1,15 @@
 require './commit_receiver'
 require 'http'
 require 'json'
+require 'byebug'
 
 cr = CommitReceiver.new
-
+message = cr.message.gsub(/\*(.*?)\n/,'')
+# .gsub(/(\r\n?|\n)/,"")
 Dotenv.load
 response = HTTP.post("https://slack.com/api/chat.postMessage", params: {
     token: ENV['SLACK_API_TOKEN'],
-    channel: '#SLACK_CHANEL',
+    channel: '#test_t',
     text: cr.message.insert(0,"```\n").insert(-1,"```"),
     as_user: true,
   })
