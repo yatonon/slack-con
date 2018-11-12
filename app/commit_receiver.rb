ee3437da98850d93d26dcd42a5c0e2b6aad15d9b
yatonon <yatonon@yahoo.co.jp>
2018-11-12 18:46:29 +0900

    rubocop 前

--- app/commit_receiver.rb
+++ app/commit_receiver.rb
@@ -4,32 +4,27 @@ require 'tapp'
 require 'date'
 require 'dotenv'
 
-# コメント取ってくる
+# comment return
 class CommitReceiver
   attr_accessor :message
   def commit_get
-
-    def make_date_diffs(*arr)
-      arr.map{ |elem| Date.today - elem }
-    end
-
+    make_date_diffs = ->(arr) {
+      arr.map { |elem| Date.today - elem }
+    }
     @message = ''
     Dotenv.load
-    client = Octokit::Client.new login: ENV['LOGIN'], password:ENV['PASSWORD']
+    client = Octokit::Client.new login: ENV['LOGIN'], password: ENV['PASSWORD']
     commitlist = client.commits ENV['REPOSITORI']
-    compare_list = Date.today.wday == 1 ? make_date_diffs(1, 2, 3) : make_date_diffs(1)
+    compare_list = make_date_diffs.call(Date.today.wday == 1 ? [1, 2, 3] : [1])
     index = 1
-
     commitlist.each do |commit|
       if compare_list.include?(commit[:commit][:author][:date].to_date)
         formated_message = commit[:commit][:message].gsub(/\*(.*?)\n/,'')
-          .gsub(/(\r\n?|\n)/,'')
+        .gsub(/(\r\n?|\n)/, '')
         author = commit[:author][:login]
         @message += "#{index}. #{formated_message}(#{author})\n"
         index += 1
         end
       end
-
   end
-
 end
